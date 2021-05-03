c618c0759270d135f36d9bec3115994a4d89b4ce
spitz linn - lspitz <linn.spitz@zhdk.ch>
2021-05-03 21:29:03 +0200

    trying to implemented different car models

---
+++ SONDER2/Assets/Scripts/06 Cars/CarVisuals.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class CarVisuals : MonoBehaviour {
+	public List<GameObject> carModels = new List<GameObject>();
+	public List<GameObject> wheels = new List<GameObject>();
+
+	public enum car { car1, car2 };
+	public car myCar;
+
+	public void SetCarModel(int i) {
+		foreach (GameObject obj in carModels) {
+			obj.SetActive(false);
+        }
+		foreach (GameObject obj in wheels) {
+			obj.SetActive(false);
+		}
+		carModels[i].SetActive(true);
+		wheels[i].SetActive(true);
+	}
+
+    private void Update() {
+		SetCarModel((int)myCar);
+    }
+}
---
+++ SONDER2/Assets/Scripts/06 Cars/Editor/CarVisualsEditor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+//[CustomEditor(typeof(CarVisuals))]
+public class CarVisualsEditor : Editor {
+	//hey bitch
+}
